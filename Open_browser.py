5284c478a063f3dd75b7bfbc00c76f22b78b9dab
ashwiinii-bm <ashwiinii.bm@gmail.com>
2025-05-27 01:48:18 -0500

    Add files via upload
    Open a chrome browser

---
+++ Open_browser.py
@@ -0,0 +1,11 @@
+
+from selenium import webdriver
+
+# Set up the WebDriver
+driver = webdriver.Chrome()  # Replace with your preferred browser
+
+# Open a website
+driver.get("https://www.google.com")
+
+# Close the browser
+# driver.quit()
